59be87b66c6f2067c4c9fbbe7a45e890ba8e4a42
Emmah Zanele Mpofu <148703284+Zaemah@users.noreply.github.com>
2023-11-24 02:51:13 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import Dictionary from "./Dictionary.js";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Dictionary />
+        <footer>
+          Coded by{" "}
+          <a
+            href="https://github.com/Zaemah"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Emmah Zanele Mpofu
+          </a>
+          , is{" "}
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
